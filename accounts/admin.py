32f625697f67f7353cf67c8ab3f69b34401187f9
moslem <moslemamiri82@gmail.com>
2025-09-21 21:47:15 +0330

    add profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
@@ -25,4 +25,29 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_first_name', 'get_last_name', 'get_email')
+    List_select_related = ('user', )
+    list_filter = ('user__username', )
+    list_per_page = 10
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    get_first_name.short_description = "First Name"
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+    get_last_name.short_description = "Last Name"
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = "Email"
+
+
+
+
+admin.site.register(Profile,ProfileAdmin)
 admin.site.register(CustomUser,CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,15 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    bio = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
