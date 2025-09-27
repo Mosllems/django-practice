6666be8b3ff977b8a95280c155616e69ca2c3184
moslem <moslemamiri82@gmail.com>
2025-09-27 18:23:10 +0330

    add serializers.py and get post_lists and post_detail

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -8,5 +8,7 @@ from . import views
 urlpatterns = [
 
     path('blog/', views.post_list, name='post_list'),
+    path('blog/<int:pk>/', views.post_detail, name='post_detail'),
+    
 
 ] 
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
+from blog.models import Post
+from .serializers import PostSerializer
 
 
 @api_view()
 def post_list(request):
-    return Response("OK")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts,many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def post_detail(request,pk):
+    post = get_object_or_404(Post,id=pk)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
