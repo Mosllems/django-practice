75f8207f48ff9192505907d533e79aa547f4b0b4
moslem <moslemamiri82@gmail.com>
2025-09-27 17:40:43 +0330

    distinguish api from html pages and set different versions for api url

---
+++ blog/api/v1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+
+
+
+
+urlpatterns = [
+
+    path('blog/', views.post_list, name='post_list'),
+
+] 
--- blog/urls.py
+++ blog/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('blog/create/', views.PostCreate.as_view(), name='post_create'),
     path('blog/<int:pk>/edit/', views.PostUpdate.as_view(), name='post_edit'),
     path('blog/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
+    path('api/v1/', include('blog.api.v1.urls')),
 
     # path('go-to-google', views.redirect_class, name='codingyar') this is for fbv
 ] 
