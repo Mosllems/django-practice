230e500da119080f61bdcb9d5ab3409af8fddfe1
moslem <moslemamiri82@gmail.com>
2025-09-30 15:44:57 +0330

    get the author from the user who is logged in and distinguish the contents from list_view and detail_view

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -14,15 +14,29 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 
-class PostSerializer(serializers.ModelSerializer):
-    # author = UserSerializer()
-    class Meta:
-        model = Post
-        fields = ['id', 'author', 'title', 'content', 'status', 'datetime_created', 'datetime_modified']
-
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'category', 'content', 'status', 'datetime_created', 'datetime_modified']
+
+
+    def create(self, validated_data):
+        request = self.context.get('request') # context be sorat pishfarz dar serializer haei ke ba modelviewset  va generic views sakhte mishavand por mishavad
+        validated_data['author'] = request.user # 1 instance az kelas customuser ra barmigardanad
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        obj = super().to_representation(instance)
+        view = self.context.get('view')
+        if view and view.action == 'list':
+            for i in ['datetime_created', 'datetime_modified']:
+                obj.pop(i)
+        obj['category'] = CategorySerializer(instance.category).data
+        return obj
