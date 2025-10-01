9f2390929c88261da4689aadbae6f8f0e34da356
moslem <moslemamiri82@gmail.com>
2025-10-01 19:56:09 +0330

    add downlodable json file url

--- config/urls.py
+++ config/urls.py
@@ -27,12 +27,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog API",
       default_version='v1',
-      description="Test description",
+      description="This is API for blog",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="moslemamiri82@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -44,7 +44,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('blog.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
-    # path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
