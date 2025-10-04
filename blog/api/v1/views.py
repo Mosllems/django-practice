4dfaaf447b097a744c46f95f0666e0203306b368
moslem <moslemamiri82@gmail.com>
2025-10-04 13:07:39 +0330

    check files with flake8

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,10 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
