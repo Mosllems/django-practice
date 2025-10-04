dec7f441965507d0d795c311aead8341a8060268
moslem <moslemamiri82@gmail.com>
2025-10-04 19:07:24 +0330

    add test for Post and Category model

---
+++ blog/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from datetime import datetime
+from accounts.models import CustomUser 
+
+from ..models import Post, Category
+
+class TestPostModel(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(
+            username = 'test',
+            password = 123
+        )
+        self.category = Category.objects.create(
+            name = 'programming'
+        )
+        return super().setUp()
+
+    def test_post_create(self):
+        post = Post.objects.create(
+            author = self.user,
+            title = 'test',
+            content = 'test',
+            status = True,
+            category = self.category,
+            # datatime_created = datetime.now(),
+            # datetime_modified = datetime.now(),
+        )
+        self.assertTrue(Post.objects.filter(pk=post.id))
+
+
+class TestCategory(TestCase):
+    def test_category_create(self):
+        category = Category.objects.create(
+            name = 'django',
+        )
+        self.assertTrue(Category.objects.filter(name=category.name))
