0b35b3d155eb9a95cf9abaaa0a40a879ab3b691d
moslem <moslemamiri82@gmail.com>
2025-10-04 20:40:28 +0330

    add setup for TestPostForm file

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -5,13 +5,15 @@ from ..models import Category
 
 
 class TestPostForm(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='programming')  
+        return super().setUp()
     
-    def test_post_form_with_date(self):
-        category = Category.objects.create(name='programming')    
+    def test_post_form_with_date(self):    
         form = PostForm(data={
             'title': 'test',
             'content': 'test',
             'status': True,
-            'category': category,
+            'category': self.category,
         })
         self.assertTrue(form.is_valid())
