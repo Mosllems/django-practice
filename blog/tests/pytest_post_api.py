1a66f8db02f17071a398460915e77c170b142ed1
moslem <moslemamiri82@gmail.com>
2025-10-05 14:56:24 +0330

    add test for creating posts

--- blog/tests/pytest_post_api.py
+++ blog/tests/pytest_post_api.py
@@ -1,12 +1,32 @@
+import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
-import pytest
+from accounts.models import CustomUser
 
+
+@pytest.fixture
+def get_user():
+    user = CustomUser.objects.create_user(username= 'tara', password= 'tara1234')
+    return user
 
 @pytest.mark.django_db
 class TestPostApi:
+    client = APIClient()
+
     def test_get_post_status_200(self):
-        client = APIClient()
         url = reverse('blog:api-v1:blog-list')
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200
+
+    def test_create_post(self, get_user):
+        url = reverse('blog:api-v1:blog-list')
+        user = get_user
+        self.client.force_login(user=user)
+        data = {
+            'author' : user.id,
+            'title' : 'test',
+            'content' : 'test',
+            'status' : True,
+        }
+        response = self.client.post(url, data=data)
+        assert response.status_code == 201
